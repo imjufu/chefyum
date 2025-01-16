56ceeb9c7a3ae29a93085ce20d83e63a4cc5c648
Julien F. <julien@jufu-dynamics.com>
2025-01-16 15:25:41 +0100

    Remove whitelisting urls for now

--- api/app/controllers/v1/auth/passwords_controller.rb
+++ api/app/controllers/v1/auth/passwords_controller.rb
@@ -6,8 +6,6 @@ class V1::Auth::PasswordsController < ApplicationController
     token = user.generate_token_for(:reset_password_token)
     redirect_url = params.fetch(:redirect_url)
 
-    return render(json: error_response([ :redirect_url_not_allowed ]), status: :unprocessable_entity) if blacklisted_redirect_url?(redirect_url)
-
     UserMailer.with(user: user, reset_password_token: token, redirect_url: redirect_url).reset_password_instructions_email.deliver_later
   end
 
@@ -23,12 +21,5 @@ class V1::Auth::PasswordsController < ApplicationController
     user.update!(password: params.fetch(:password))
 
     render json: success_response(user: user)
-  end
-
-  private
-
-  def blacklisted_redirect_url?(redirect_url)
-    redirect_whitelist = [ "http://localhost:4000/password/edit" ]
-    !redirect_whitelist.include?(redirect_url)
   end
 end
--- api/app/controllers/v1/auth/unlocks_controller.rb
+++ api/app/controllers/v1/auth/unlocks_controller.rb
@@ -9,8 +9,6 @@ class V1::Auth::UnlocksController < ApplicationController
     token = user.generate_token_for(:unlock_token)
     redirect_url = params.fetch(:redirect_url)
 
-    return render(json: error_response([ :redirect_url_not_allowed ]), status: :unprocessable_entity) if blacklisted_redirect_url?(redirect_url)
-
     UserMailer.with(user: user, unlock_token: token, redirect_url: redirect_url).unlock_instructions_email.deliver_later
   end
 
@@ -19,12 +17,5 @@ class V1::Auth::UnlocksController < ApplicationController
     user.unlock_access!
 
     redirect_to URI(params.fetch(:redirect_url))
-  end
-
-  private
-
-  def blacklisted_redirect_url?(redirect_url)
-    redirect_whitelist = [ "http://localhost:4000/unlock" ]
-    !redirect_whitelist.include?(redirect_url)
   end
 end
