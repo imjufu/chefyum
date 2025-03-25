8fcbcb79700deacd58541fa8ff6d3cd377b98383
Julien F. <julien@jufu-dynamics.com>
2025-03-25 14:31:00 +0100

    Return more data in /me

--- api/app/controllers/v1/me_controller.rb
+++ api/app/controllers/v1/me_controller.rb
@@ -1,6 +1,8 @@
 class V1::MeController < ApplicationController
   def index
-    render json: success_response(@current_user)
+    render json: success_response(@current_user.as_json(
+      { only: [ :id, :name, :email, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip ] }
+    ))
   end
 
   def update
