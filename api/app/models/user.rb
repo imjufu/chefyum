bb6c499b40d3263541d0e89b6238292d3a20f398
Julien F. <julien@jufu-dynamics.com>
2025-04-10 15:07:42 +0200

    Improve User.as_json method

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -2,26 +2,41 @@ class User < ApplicationRecord
   include Authenticatable
 
   validates :name, presence: true
+  validates :activity_level, inclusion: { in: CalorieCalculator::ACTIVITY_LEVELS.keys.map(&:to_s) }, allow_blank: true
+  validates :gender, inclusion: { in: CalorieCalculator::GENDERS.keys.map(&:to_s) }, allow_blank: true
+  validates :height_in_centimeters, :weight_in_grams, numericality: { only_integer: true }, allow_blank: true
 
   def age
     AgeCalculator.new(birthdate).calculate
   end
 
-  def macro_calc
-    return @macro_calc if @macro_calc
+  def macro
+    return @macro if @macro
 
-    macro_calc = MacroCalculator.new(
+    macro_calculator = MacroCalculator.new(
       gender:,
       birthdate:,
       height_in_centimeters:,
-      weight_in_kilograms: weight_in_grams / 1000,
+      weight_in_kilograms: weight_in_grams ? weight_in_grams / 1000 : nil,
       activity_level:
     )
 
-    @macro_calc = macro_calc.calculate ? macro_calc : false
+    @macro = macro_calculator.calculate
   end
 
-  def as_json(options = nil)
-    super({ only: [ :id, :name, :email ] }.merge(options || {}))
+  def as_json(options = nil, with_security_data: false, with_macro_data: false)
+    attrs = [ :id, :name, :email, :unconfirmed_email, :gender, :birthdate, :height_in_centimeters, :weight_in_grams, :activity_level ]
+    if with_security_data
+      attrs += [ :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip ]
+    end
+    if with_macro_data && macro
+      options ||= {}
+      if options.include? :methods
+        options[:methods] += :macro
+      else
+        options[:methods] = :macro
+      end
+    end
+    super({ only: attrs }.merge(options || {}))
   end
 end
