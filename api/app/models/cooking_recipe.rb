af597d991bed825d95c53cd2fa1ba39850598417
Julien F. <julien@jufu-dynamics.com>
2025-04-22 10:40:38 +0200

    Rename calculate_nutritional_values into nutritional_values

--- api/app/models/cooking_recipe.rb
+++ api/app/models/cooking_recipe.rb
@@ -3,8 +3,10 @@ class CookingRecipe < ApplicationRecord
 
   validates :title, :description, :steps, presence: true
 
-  def calculate_nutritional_values
-    nutritional_values = {
+  def nutritional_values
+    return @nutritional_values if @nutritional_values
+
+    @nutritional_values = {
       energy: 0,
       proteins: 0,
       carbohydrates: 0,
@@ -15,10 +17,30 @@ class CookingRecipe < ApplicationRecord
       fibers: 0
     }
     ingredients.each do |ingredient|
-      nutritional_values.keys.each do |k|
-        nutritional_values[k] += ((ingredient.food.nutrition_facts[k.to_s] || 0) * ingredient.quantity / 100).round(2)
+      @nutritional_values.keys.each do |k|
+        @nutritional_values[k] += ((ingredient.food.nutrition_facts[k.to_s] || 0) * ingredient.quantity / 100).round(2)
       end
     end
-    nutritional_values
+    @nutritional_values
+  end
+
+  def as_json(options = nil, with_ingredients: false, with_nutritional_values: false)
+    attrs = [ :id, :title, :description, :steps ]
+    options ||= {}
+    if with_ingredients
+      if options.include? :include
+        options[:include] += :ingredients
+      else
+        options[:include] = :ingredients
+      end
+    end
+    if with_nutritional_values
+      if options.include? :methods
+        options[:methods] += :nutritional_values
+      else
+        options[:methods] = :nutritional_values
+      end
+    end
+    super({ only: attrs }.merge(options || {}))
   end
 end
