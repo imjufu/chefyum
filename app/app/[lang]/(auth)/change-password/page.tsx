00606ccdf9c56412d0f5cd65626eb99e5cb4ee01
Julien F. <julien@jufu-dynamics.com>
2025-05-13 14:54:43 +0200

    Fix: import default function

--- app/app/[lang]/(auth)/change-password/page.tsx
+++ app/app/[lang]/(auth)/change-password/page.tsx
@@ -3,7 +3,7 @@
 import { useContext } from "react";
 import { AuthContext, IntContext } from "@/app/lib/providers";
 import { redirect } from "next/navigation";
-import { Ui } from "./ui";
+import Ui from "./ui";
 
 export default function Page() {
   const { currentSession } = useContext(AuthContext);
